1c358f5d69b8ff5f51ab7c82cc03e5eda1b40a76
Oleg Stotsky <ostotsky@gmail.com>
2019-10-09 12:54:21 +0300

    hw5: add task2

--- hw5/2.hs
+++ hw5/2.hs
@@ -1,5 +1,9 @@
-data Nat = Zero | Suc Nat
+data Nat = Zero | Suc Nat deriving (Show)
 
 foldNat :: (a -> a) -> a -> Nat -> a
 foldNat _ acc (Zero) = acc
 foldNat f acc (Suc x) = foldNat f (f acc) x
+
+sumNat :: Nat -> Nat -> Nat
+sumNat a b = foldNat suc b a
+           where suc x = Suc x
