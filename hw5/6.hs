d40d4bc7c0a016503a016b0cb49d4a91ddb59380
Oleg Stotsky <ostotsky@gmail.com>
2019-10-09 13:05:43 +0300

    fix task6

--- hw5/6.hs
+++ hw5/6.hs
@@ -4,4 +4,4 @@ data Term = Var String | Application Term Term | Abstraction Arg Term
 isFreeIn :: String -> Term -> Bool
 isFreeIn var (Var x) = var == x
 isFreeIn var (Application x y) = isFreeIn var x || isFreeIn var y
-isFreeIn var (Abstraction (Arg x) y) = not (var == x) && isFreeIn var y
+isFreeIn var (Abstraction (Arg x) y) = var /= x && isFreeIn var y
