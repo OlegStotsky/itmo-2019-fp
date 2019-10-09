f7b48e179a3fdc4d31df2be366d81cd4e117b7ef
Oleg Stotsky <ostotsky@gmail.com>
2019-10-09 12:42:36 +0300

    hw5: fix task3

--- hw5/3.hs
+++ hw5/3.hs
@@ -1,4 +1,4 @@
 lexLess :: (Ord a) => [a] -> [a] -> Bool
-lexLess (x:xs) (y:ys) = x < y || not (x == y) && lexLess xs ys
 lexLess [] (_:_) = True 
+lexLess (x:xs) (y:ys) = x < y || not (x > y) && lexLess xs ys
 lexLess _ _ = False
