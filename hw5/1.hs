0c1fcffd64544126a4c3ebf2faeb08b7a9fbdb85
Oleg Stotsky <ostotsky@gmail.com>
2019-10-09 12:44:35 +0300

    hw5: fix task1

--- hw5/1.hs
+++ hw5/1.hs
@@ -1,6 +1,6 @@
 data IntTree = Leaf | Node Int [IntTree]
 
 sumIntTree :: IntTree -> Integer
-sumIntTree (Leaf) = 0
+sumIntTree Leaf = 0
 sumIntTree (Node val children) = fromIntegral val + sumOfChildren
-                               where sumOfChildren = (foldl (+) 0 $ map sumIntTree children)
+                               where sumOfChildren = sum $ map sumIntTree children
