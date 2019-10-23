e50ecc4c6c288cad6da1e25847c64eb3398951a8
Oleg Stotsky <ostotsky@gmail.com>
2019-10-23 13:36:26 +0300

    fix 4 and 7

--- hw7/4.hs
+++ hw7/4.hs
@@ -1,4 +1,5 @@
 incrementBy :: (Enum a, Enum b) => a -> b -> b -> a
-incrementBy base from to = _incrementBy base from to $ length $ enumFromTo from to
-                            where _incrementBy base from to 0 = pred base
-                                  _incrementBy base from to n = _incrementBy (succ base) from to (n-1) 
+incrementBy base from to = helper base from to $ length $ enumFromTo from to
+                            where helper base from to 1 = base
+                                  helper base from to 0 = base
+                                  helper base from to n = helper (succ base) from to (n-1) 
--- hw7/7.hs
+++ hw7/7.hs
@@ -4,5 +4,5 @@ f g = fmap (\tuple -> fmap (\list -> fmap (\mb -> fmap g mb) list) tuple)
 f' :: (a -> b) -> Maybe (e, [Maybe a]) -> Maybe (e, [Maybe b])
 f' = fmap . fmap . fmap . fmap
 
-g :: (a -> b) -> Maybe (e, [[Maybe a]]) -> Maybe (e, [[Maybe b]])
-g = fmap . fmap . fmap . fmap . fmap
+g :: (a -> b) -> (e, [[Maybe a]]) -> (e, [[Maybe b]])
+g = fmap . fmap . fmap . fmap
