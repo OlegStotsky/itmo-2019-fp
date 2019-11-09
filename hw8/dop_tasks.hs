88103bcb73036fdd2ce24b62babf26271d0b1de1
Oleg Stotsky <ostotsky@gmail.com>
2019-11-09 16:58:10 +0300

    update dop tasks

--- hw8/dop_tasks.hs
+++ hw8/dop_tasks.hs
@@ -1,8 +1,10 @@
+import Data.Bool
+
 sumOfSquares :: [Integer] -> Integer
 sumOfSquares = foldr (\val acc -> acc + val^2) 0
 
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' p = foldr (\val acc -> if p val then val : acc else acc) []
+filter' p = foldr (\val acc -> bool  acc (val : acc) (p val)) []
 
 zip' :: [a] -> [b] -> [(a, b)]
 zip' xs ys = reverse $ fst $ foldl (\(result, (y:ys)) val -> ((val, y):result, ys)) ([], ys) xs
