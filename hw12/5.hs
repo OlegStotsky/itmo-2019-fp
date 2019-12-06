ad211858035f74fa0ae22b1392ea5cb895fb38bd
Oleg Stotsky <ostotsky@gmail.com>
2019-12-06 21:10:54 +0300

    add task5

---
+++ hw12/5.hs
@@ -0,0 +1,36 @@
+import Control.Monad.State
+
+writerState :: Monoid b => (a, b) -> State b a
+writerState (a, b) = state $ \s -> (a, s <> b)
+
+tellState :: Monoid a => a -> State a ()
+tellState x = writerState ((), x)
+
+listenState :: Monoid a => State a b -> State a (b, a)
+listenState st = do
+  x <- st
+  s <- get
+  return (x, s)
+
+listensState :: Monoid a => (a -> w) -> State a b -> State a (b, w)
+listensState f st = do
+  x <- st
+  s <- get
+  return (x, f s)
+
+censorState :: Monoid a => (a -> a) -> State a b -> State a b
+censorState f st = do
+  oldS <- get
+  put mempty
+  val <- st
+  newS <- get
+  put (oldS <> f newS)
+  return val
+
+test = do 
+  a <- writerState ("y", "x")
+  tellState "y"
+  tellState "z"
+  ((), w) <- listenState $ tellState "z"
+  censorState (take 1) $ tellState "yy"
+  pure a
